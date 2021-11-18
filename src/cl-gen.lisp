fba10e411445cbe9338c89c9cfa36aa6a413ab14
Carlo Sganzerla <carlosganzrl@hotmail.com>
2021-11-18 20:10:30 -0300

    Added new capabilities, updated examples

--- src/cl-gen.lisp
+++ src/cl-gen.lisp
@@ -4,24 +4,29 @@
   `(let (,@(mapcar (lambda (s) `(,s (gensym))) syms))
      ,@body))
 
+(defun mklist (obj)
+  (if (consp obj)
+      obj
+      (list obj)))
+
 (defun out-of-context-error ()
   (error "Cannot call yield or stop outside of a generator context"))
 
-(defvar *stop* (lambda (&optional x) 
+(defvar *stop* (lambda (&rest x)
                  (declare (ignore x))
                  (out-of-context-error)))
 
-(defvar *next* (lambda (&optional x) 
+(defvar *next* (lambda (&rest x)
                  (declare (ignore x))
                  (out-of-context-error)))
 
 (defvar *restarts* nil)
 
-(defun stop (&optional x)
-  (funcall *stop* x))
+(defun stop (&rest x)
+  (apply *stop* x))
 
-(defun next (&optional x)
-  (funcall *next* x))
+(defun next (&rest x)
+  (apply *next* x))
 
 (defmacro stop-when (test-from &body body)
   (with-gensyms (body-eval)
@@ -35,29 +40,38 @@
        (let ((,body-eval (progn ,@body)))
          (stop ,body-eval)))))
 
-(defun yield (value)
+(defun yield (&rest values)
   (if (car *restarts*)
-      (invoke-restart (car *restarts*) value (cdr *restarts*))
+      (apply #'invoke-restart (car *restarts*) (cdr *restarts*) values)
       (out-of-context-error)))
 
-(defmacro generator-bind ((binding generator-form) &body body) 
+(defmacro generator-bind (bindings
+                          (generator
+                            &optional (return-form nil return-form-supplied-p))
+                          &body body)
   (with-gensyms (whole-block restart lambda-block)
     `(block ,whole-block
-            (restart-bind 
-              ((,restart (lambda (,binding *restarts*)
+            (restart-bind
+              ((,restart (lambda (*restarts* ,@bindings)
                            (block
                              ,lambda-block
-                             (let ((*stop* (lambda (x) 
-                                             (return-from ,whole-block x)))
-                                   (*next* (lambda (x)
-                                             (return-from ,lambda-block x)))) 
+                             (let ((*stop*
+                                     (lambda (&rest x)
+                                       (return-from ,whole-block
+                                                    (apply #'values x))))
+                                   (*next*
+                                     (lambda (&rest x)
+                                       (return-from ,lambda-block
+                                                    (apply #'values x)))))
                                ,@body)))))
-              (let ((*restarts* (cons ',restart *restarts*))) 
-                ,generator-form)))))
+              (let ((*restarts* (cons ',restart *restarts*)))
+                ,@(if return-form-supplied-p
+                      `(,generator ,return-form)
+                      `(,generator)))))))
 
-(defmacro generator-collect ((binding generator-form) &body body)
+(defmacro generator-collect (bindings generator-form &body body)
   (with-gensyms (lst)
     `(let (,lst)
-       (generator-bind (,binding ,generator-form)
+       (generator-bind ,bindings (,generator-form)
          (push (progn ,@body) ,lst))
        (nreverse ,lst))))
--- src/examples.lisp
+++ src/examples.lisp
@@ -9,40 +9,39 @@
 (defun js-test ()
   (yield 1)
   (yield 2)
-  (yield 3)
+  (yield 3 5 6)
+  ; you can yield multiple values, but the consumer must be ready for it
   (print "shouldnt call me!")
   (yield 4)
   (yield 5))
 
-(generator-bind (x (js-test))
-  (stop-when (= x 3) x)) ; Stop generating when X is 3, and return X
+(generator-bind (x &rest stuff) ((js-test))
+  (stop-when (= x 3) stuff)) ; Stop generating when X is 3, and return X
 
-;;; Sequential number generators
+;; Sequential number generators
 
 (defun numbers ()
-  (do ((x 0 (1+ x))) 
+  (do ((x 0 (1+ x)))
       (nil)
       (yield x)))
 
 (defun even-numbers ()
-  (generator-bind (x (numbers))
+  (generator-bind (x) ((numbers))
     (when (evenp x)
       (format t "Yield returns ~A~%" (yield x)))))
 
-(generator-bind (x (even-numbers))
-    (format t "Yield supplies ~A~%" x)
-    (stop-when (>= x 100) 'ok)
-    (- 100 x)) ; Yield returns the value of the body of GENERATOR-BIND
+(generator-bind (x) ((even-numbers))
+  (format t "Yield supplies ~A~%" x)
+  (stop-when (>= x 100) 'ok)
+  (- 100 x)) ; Yield returns the value of the body of GENERATOR-BIND
 
 ;;; List operations
 (defun sum-generation (lst)
   (reduce (lambda (acc x) (yield (+ x acc))) lst))
 
 ;; The lambda body of SUM-GENERATION is "forwarded" to the body
-(generator-bind (e (sum-generation '(1 2 3 4 5)))
+(generator-bind (e) ((sum-generation '(1 2 3 4 5)))
   (print e))
-
-;;; Tree traversal
 
 ;; Generic tree traversal generator
 (defun tree-generator (tree)
@@ -55,7 +54,7 @@
 ;; Generator collect will collect all results on a list unless you explicitly
 ;; skip an iteration
 (defun find-tree (pred tree)
-  (generator-collect (e (tree-generator tree))
+  (generator-collect (e) (tree-generator tree)
     (if (funcall pred e)
         e
         (next))))
@@ -64,11 +63,38 @@
 
 ;; Maps TREE onto FN and flats it
 (defun tree-map (fn tree)
-  (generator-collect (e (tree-generator tree))
+  (generator-collect (e) (tree-generator tree)
     (funcall fn e)))
 
 ;; For trolling purposes only
-(tree-map (lambda (x) (intern (nreverse (symbol-name x))))  
-          '(as long as people use (common) lisp there will be (necessarily) 
-               wars (albert einstein (who we found out later that was 
+(tree-map (lambda (x) (intern (nreverse (symbol-name x))))
+          '(as long as people use (common) lisp there will be (necessarily)
+               wars (albert einstein (who we found out later that was
                                           actually lionel messi))))
+
+;; Yield can return multiple values
+(defun subsequent-elements (lst)
+  (mapc #'yield  lst (cdr lst)))
+
+;; You may change the return form of a generator
+;;(the default is the return of the generator itself)
+;; You may return multiple values from stop and the generator form also.
+(defun sortedp (lst)
+  (generator-bind (x y) ((subsequent-elements lst) (values t lst))
+    (when (> x y)
+      (stop nil (list x y)))))
+
+(sortedp (list 1 0 3 4 5))
+(sortedp (list 1 3 9 12 12 13))
+(sortedp nil)
+
+;; Next can also return multiple values
+(defun weird-mapcan (lst)
+  (mapcan (lambda (x)
+            (multiple-value-bind (x y) (yield x)
+              (list x y)))
+          (copy-list lst)))
+
+(generator-bind (x) ((weird-mapcan '(1 2 3 4 5)))
+  ;; Same as values in this case
+  (next x (format nil "~R" x)))
