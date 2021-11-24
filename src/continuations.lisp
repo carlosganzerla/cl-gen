e7c555670d60082cc0d579931375233e36cf39cf
Carlo Sganzerla <carlosganzrl@hotmail.com>
2021-11-24 19:23:16 -0300

    Getting it righttt

--- src/continuations.lisp
+++ src/continuations.lisp
@@ -4,60 +4,75 @@
   `(lambda (&rest x)
      (return-from ,block-name (apply #'values x))))
 
-(defmacro with-continuation-context (&body body)
+(defmacro with-cc-context (&body body)
   (with-gensyms (block)
     `(block ,block
             (let (($cc #'identity)
                   ($stop ,(return-lambda block)))
               ,@body))))
 
-(defuncont return-cont (&rest args)
+(defuncont stop (&rest args)
   (apply $stop args))
 
-(defuncont continuation (&rest args)
+(defuncont cc (&rest args)
   (apply $cc args))
 
 (defmacro defuncont (name args &body body)
   (let ((f (intern (concat "%" name))))
     `(progn
        (defmacro ,name (&rest args)
-         `(,',f $cc ,@args))
-       (defun ,f ($cc ,@args) 
-         (declare (ignorable $cc))
+         `(,',f $cc $stop ,@args))
+       (defun ,f ($cc $stop ,@args) 
+         (declare (ignorable $cc $stop))
          ,@body))))
 
-(defmacro continuation-bind (bindings form &body body)
+(defmacro cc-bind (bindings form &body body)
   (with-gensyms (block)
     `(let (($cc (lambda ,bindings 
                   (block ,block 
                          (let (($stop ,(return-lambda block)))
+                           (declare (ignorable $stop))
                            ,@body)))))
+       (declare (ignorable $cc))
        ,form)))
 
 (defuncont printy (x)
-  (progn (format t "Printy ~A~%" x) (continuation x)))
+  (progn (format t "Printy ~A~%" x) (cc x)))
 
 (defuncont prinky (x)
-  (progn (format t "Prinky ~A~%" x) (continuation x)))
+  (progn (format t "Prinky ~A~%" x) (cc x)))
 
 (defuncont prinpy (x)
-  (progn (format t "Prinpy ~A~%" x) (continuation x)))
+  (progn (format t "Prinpy ~A~%" x) (cc x)))
 
 
 (defuncont bazzie (n)
-  (continuation-bind (x) (progn (print $cc) (continuation 3))
+  (cc-bind (x) (progn (print $cc) (cc 3))
     (print "evaled x")
-    (continuation-bind (y) (prinky (1+ n))
+    (cc-bind (y) (prinky (1+ n))
       (print "evaled y")
-      (continuation-bind (z) (prinpy (1- n))
+      (cc-bind (z) (prinpy (1- n))
         (print "evaled z")
-        (format t "albierto tien ~A ~A ~A garrafitas~%" x y z)))))
+        (format t "albierto tien ~A ~A ~A garrafitas~%" x y z))
+      (cc 5))))
 
 (defuncont c ()
-  (continuation-bind (x) (progn (print $cc) (continuation 3))
+  (cc-bind (x) (bazzie 5)
     (print 'eae)
     (print "evaled x")
     x))
 
-(with-continuation-context (c))
-(with-continuation-context (bazzie 5))
+(with-cc-context (c))
+
+(defuncont boris ()
+  (cc-bind (x) $cc
+    (format t "evaled x ~A~%" x)
+    (cc-bind (y) $cc
+      (format t "evaled y ~A~%" y)
+      (stop 33)
+      (cc-bind (z) $cc
+        (format t "evaled z ~A~%" z)
+        (print $cc)
+        (format t "gameover son~%")))))
+
+(with-cc-context (boris))
